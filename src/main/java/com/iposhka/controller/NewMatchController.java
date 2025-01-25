bc4d2ce452fd4224349886e430de1d7eaf8a0ac3
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-01-25 19:10:12 +0300

    add redirect in NewMatchController

--- src/main/java/com/iposhka/controller/NewMatchController.java
+++ src/main/java/com/iposhka/controller/NewMatchController.java
@@ -1,6 +1,8 @@
 package com.iposhka.controller;
 
 import com.iposhka.exception.InvalidDataException;
+import com.iposhka.model.OngoingMatch;
+import com.iposhka.service.OngoingMatchesService;
 import com.iposhka.util.JspHelper;
 import com.iposhka.util.Validator;
 import jakarta.servlet.ServletException;
@@ -10,10 +12,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/new-match")
 public class NewMatchController extends HttpServlet {
-
+    private final OngoingMatchesService ongoingMatchesService = OngoingMatchesService.getInstance();
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.getRequestDispatcher(JspHelper.getPath("new-match")).forward(req, resp);
@@ -23,9 +26,16 @@ public class NewMatchController extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         try{
             req.removeAttribute("error");
+
             String firstPlayer = req.getParameter("firstPlayer");
             String secondPlayer = req.getParameter("secondPlayer");
+
             Validator.checkNames(firstPlayer, secondPlayer);
+
+            //Need createOrGetPlayer method
+
+            UUID id = ongoingMatchesService.createMatch(firstPlayer, secondPlayer);
+            resp.sendRedirect(req.getContextPath() + "/match-score?uuid=" + id);
         }catch(InvalidDataException e){
             req.setAttribute("error", e.getMessage());
             req.getRequestDispatcher(JspHelper.getPath("new-match")).forward(req, resp);
