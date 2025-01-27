7c9341ba35f166a9614446431806cd2bf28050fc
Yaroslav Kelesh <yaroslavkusb@gmail.com>
2025-01-27 17:53:17 +0300

    Feat: calculation score service is finalize

--- src/main/java/com/iposhka/service/MatchScoreCalculationService.java
+++ src/main/java/com/iposhka/service/MatchScoreCalculationService.java
@@ -1,23 +1,81 @@
 package com.iposhka.service;
 
 import com.iposhka.dto.OngoingMatch;
+import com.iposhka.dto.Score;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MatchScoreCalculationService {
     private static final MatchScoreCalculationService INSTANCE = new MatchScoreCalculationService();
+    private static final int POINTS_TO_WIN_GAME = 5;
+    private static final int GAMES_TO_WIN_SET = 6;
+    private static final int TIEBREAK_POINTS_TO_WIN = 7;
+    private static final int MIN_ADVANTAGE = 2;
 
     public void calculateScore(OngoingMatch match, Integer winnerId){
-        if(match.getPlayer1().getId().equals(winnerId)){
-            match.getPlayer1Score().wonPoint();
+        Score winnerScore =  match.getPlayer1().getId().equals(winnerId)
+                ? match.getPlayer1Score()
+                : match.getPlayer2Score();
+
+        Score opponentScore = match.getPlayer1().getId().equals(winnerId)
+                ? match.getPlayer2Score()
+                : match.getPlayer1Score();
+
+        winnerScore.wonPoint();
+
+        if(isTieBreak(winnerScore, opponentScore)){
+            checkTieBreak(winnerScore, opponentScore);
         }else{
-            match.getPlayer2Score().wonPoint();
+            checkGameResult(winnerScore, opponentScore);
+            checkSetResult(winnerScore, opponentScore);
         }
     }
 
+
     public boolean isMatchOver(OngoingMatch match){
-        return false;
+        return match.getPlayer1Score().getSets() > MIN_ADVANTAGE || match.getPlayer2Score().getSets() > MIN_ADVANTAGE;
+    }
+
+    private void checkGameResult(Score winnerScore, Score opponentScore){
+        if (winnerScore.getPoints() >= POINTS_TO_WIN_GAME
+                && winnerScore.getPoints() - opponentScore.getPoints() >= MIN_ADVANTAGE) {
+            winnerScore.wonGame();
+            clearPoints(winnerScore, opponentScore);
+        }
+    }
+
+    private void checkSetResult(Score winnerScore, Score opponentScore){
+        if (winnerScore.getGames() >= GAMES_TO_WIN_SET
+                && winnerScore.getGames() - opponentScore.getGames() >= MIN_ADVANTAGE) {
+            winnerScore.wonSet();
+            clearGames(winnerScore, opponentScore);
+        }
+    }
+
+    private void checkTieBreak(Score winnerScore, Score opponentScore) {
+        if(winnerScore.getPoints() >= TIEBREAK_POINTS_TO_WIN
+        && winnerScore.getPoints() - opponentScore.getPoints() >= MIN_ADVANTAGE){
+            winnerScore.wonSet();
+            clearGames(winnerScore, opponentScore);
+            clearPoints(winnerScore, opponentScore);
+        }
+    }
+
+    private void clearGames(Score... scores){
+        for (Score score : scores) {
+            score.clearGames();
+        }
+    }
+
+    private void clearPoints(Score... scores){
+        for (Score score : scores) {
+            score.clearPoints();
+        }
+    }
+
+    private boolean isTieBreak(Score player1, Score player2) {
+        return player1.getGames() == GAMES_TO_WIN_SET && player2.getGames() == GAMES_TO_WIN_SET;
     }
 
     public static MatchScoreCalculationService getInstance(){
